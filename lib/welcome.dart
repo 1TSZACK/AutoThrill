2bc5f70b46ae7058c5ec426f25599442782fe47a
1TSZACK <95505665+1TSZACK@users.noreply.github.com>
2021-12-04 04:34:08 +0500

    Update welcome.dart

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,12 +1,27 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
+import 'package:project/widgets/navigation_drawer_widget.dart';
 
-class Welcome extends StatelessWidget {
+class Welcome extends StatefulWidget {
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Welcome!'),
-    );
+        color: Colors.grey[800],
+        child: Scaffold(
+          drawer: NavigationDrawerWidget(),
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            //title: Text('Welcome'),
+            //automaticallyImplyLeading: false,
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+          ),
+        ));
   }
 }
